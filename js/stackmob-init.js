febcfca1dd5d506ff4dcdf9d10f12e13a452fff7
Sidney Maestre <sid@crushitmobile.com>
2013-05-06 13:27:15 -0700

    detect if dev or production and use correct keys.

--- js/stackmob-init.js
+++ js/stackmob-init.js
@@ -1,8 +1,19 @@
 define(['stackmob'], function(StackMob) {
-    StackMob.init({
-	    publicKey:  "454b65bb-ad94-4699-9f3f-5e3b90b13370",
-	    apiVersion: 0
-	});
+    
+	if(window.location.href === "http://hollagram_html5_requirejs.stackmob339.stackmobapp.com") {
+		StackMob.init({
+	    	publicKey:  "e3cbec6f-4ecc-4b2f-88e2-e8f4d4f13731",
+	    	apiVersion: 1
+		});
+		console.log('init production');
+	} else {
+		StackMob.init({
+		    publicKey:  "454b65bb-ad94-4699-9f3f-5e3b90b13370",
+		    apiVersion: 0
+		});
+		console.log('init dev');
+	}
+    
     // return a particular StackMob that we've initialised
     return StackMob;
 });
